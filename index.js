cb1fa4afdd410d3841a7927e47dd57fb588ce6a3
sri-vatsann <srivatsan2910@gmail.com>
2025-07-18 09:37:27 +0530

    added search function

--- index.js
+++ index.js
@@ -9,3 +9,28 @@ menuicon.addEventListener("click", function(){
 x.addEventListener("click", function(){
     sidenav.style.right="-50%"
 })
+
+//Product search functionality
+var productContainer = document.getElementById("product-container")
+var search = document.getElementById("search")
+
+var productlist = productContainer.querySelectorAll("div")
+
+console.log(productlist)
+
+search.addEventListener("keyup",function(){
+
+    var enteredvalue=event.target.value.toUpperCase()
+
+    for(count=0; count<productlist.length; count++){
+
+        var productname=productlist[count].querySelector("h1").textContent
+
+        if(productname.toUpperCase().indexOf(enteredvalue)<0){
+            productlist[count].style.display="none"
+        }
+        else{
+             productlist[count].style.display="block"
+        }
+    }   
+})
